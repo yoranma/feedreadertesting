34f39b070751c7cae6a9349b167e63b85deefff5
YoranMa <349316972@qq.com>
2017-12-28 22:15:10 +0800

    test example

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -25,29 +25,53 @@ $(function() {
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          */
+		 it('url are not empty',function(){
+			allFeeds.forEach(function(item){
+				expect(item.url).toBeDefined();
+				expect(item.url.length).not.toBe(0);
+			});
+		 });
 
 
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
          */
+		 it('name are not empty',function(){
+			 allFeeds.forEach(function(item){
+				 expect(item.name).toBeDefined();
+				 expect(item.name.length).not.toBe(0);
+			 });
+		 });
     });
 
 
     /* TODO: 写一个叫做 "The menu" 的测试用例 */
+	describe('The menu',function(){
 
+		var bodymenu;
+		beforeEach(function(){
+			bodymenu = $('body');
+		});
         /* TODO:
          * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
          * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
+		 it('menu hide as default',function(){
+			 expect(bodymenu.hasClass('menu-hide1')).toBeTruthy();			 
+		 });
 
          /* TODO:
           * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
           * 测试应该包含两个 expectation ： 党点击图标的时候菜单是否显示，
           * 再次点击的时候是否隐藏。
           */
-
+		  it('menu click',function(){
+			  
+		  });
+	});
+	
     /* TODO: 13. 写一个叫做 "Initial Entries" 的测试用例 */
-
+    describe('Initial Entries',function(){
         /* TODO:
          * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
          * 里面至少有一个 .entry 的元素。
@@ -55,11 +79,12 @@ $(function() {
          * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
          * 和异步的 done() 函数。
          */
-
+    });
     /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
-
+	describe('New Feed Selection',function(){
         /* TODO:
          * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
          * 记住，loadFeed() 函数是异步的。
          */
+	});
 }());
